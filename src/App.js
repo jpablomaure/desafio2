fb149630876d3fb536a9e3e27bf0f97fd91cfc0d
jpablomaure <jpablomaure@gmail.com>
2022-05-16 17:51:34 -0300

    desafio 2 corregido app.js y limpieza de listado de productos

--- src/App.js
+++ src/App.js
@@ -2,24 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div classNameNameName="App">
-        <nav className="navbar navbar-expand-sm navbar-light ">
-        <a className="navbar-brand" href="../public/index.html"> <img src="../public/img/logo.png" heigth="96px" width="170px"
-                alt="logo"></a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-                <a className="btn btn-sm btn-outline-secondary" href="#">HOME</a>
-                <a className="btn btn-sm btn-outline-secondary" href="#">NOSOTROS</a>
-                <a className="btn btn-sm btn-outline-secondary" href="#">TIENDA</a>
-                <a className="btn btn-sm btn-outline-secondary" href="#">SERVICIOS</a>
-                <a className="btn btn-sm btn-outline-secondary" href="#">CONTACTO</a>
-            </div>
-        </div>
-    </nav>
+    <div className="App">
+        <NavBar />
     </div>
   );
 }
