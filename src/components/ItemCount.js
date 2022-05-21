df0fca6642ab1acab4aaf27c91943c2222b43d46
jpablomaure <jpablomaure@gmail.com>
2022-05-21 10:51:10 -0300

    desafio con boton

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -4,33 +4,15 @@ import Button from 'react-bootstrap/Button';
 const ItemCount = ({stock , initial }) => {
     const [valor, setValor] = useState(initial);
 
-    const [stockActual, setStockActual] = useState(stock);
-
-    const [stockActualPost , setStockActualPost] = useState(stockActual - initial);
-
-    
-    
-    console.log('stock disponible al comenzar '+ stockActual);
-    console.log('stock disponible al comenzar post '+ stockActualPost);
-    console.log('valor '+ valor);
-
-
-
     const increment = () => {
         if (valor < stock) {
             setValor(valor+1);
-            setStockActual(stockActual - valor);
-            setStockActualPost(stockActual - valor);
-            console.log('stock disponible luego de sumar '+ stockActual);
         };
     }
 
     const decrement = () => {
         if (valor > initial) {
             setValor(valor-1);
-            setStockActual(stockActual + valor);
-            setStockActualPost(stockActual - valor);
-            console.log('stock disponible luego de restar '+ stockActual);
         };
         
     }
@@ -39,8 +21,7 @@ const ItemCount = ({stock , initial }) => {
     return (
         <>
             <div>
-                <p>Stock disponible : {stockActual}</p>
-                <p>Stock disponible luego de esta compra : {stockActualPost}</p>
+                <p>Stock disponible : {stock}</p>
             </div>
             <div>
                 <Button onClick={decrement} variant="primary" size="lg">-</Button>{' '}
